58002008cb193acf8c0d334c7fc80aeda29034fd
Amaan <118141092+Amaan1628@users.noreply.github.com>
2025-02-24 03:19:49 +0530

    added header and logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,8 @@
-// import chefClaudeLogo from "./images/chef-claude-icon.png"
-
-// export default function Header() {
-//     return (
-//         <header>
-//             <img src={chefClaudeLogo}/>
-//             <h1>Chef Claude</h1>
-//         </header>
-//     )
-// }
+export default function Header() {
+    return (
+        <header>
+            <img src="AI-chef.webp"/>
+            <h1 className="chef-header">Chef Claude</h1>
+        </header>
+    )
+}
